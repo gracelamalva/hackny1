69f9cf093e793adce098347638ad251faaababc1
Malidrisi <mahalidrisi@gmail.com>
2018-04-07 22:16:55 -0400

    updated

--- food.py
+++ food.py
@@ -1,6 +1,10 @@
 
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
+#from BeautifulSoup import BeautifulSoup
+#import unirest
+import json
+import urllib.request as urllib2
 
 app = ClarifaiApp()
 model = app.models.get('food-items-v1.0')
@@ -21,8 +25,22 @@ def get_ingredients(image_url):
         return ingredients
 
 def main():
-    print(get_ingredients('http://onelittleproject.com/wp-content/uploads/2016/07/DSC_9218.jpg'))
-    
+    #l = get_ingredients('http://onelittleproject.com/wp-content/uploads/2016/07/DSC_9218.jpg')
+    l = get_ingredients('https://images-na.ssl-images-amazon.com/images/I/512l5vJviXL._SY450_.jpg')
+    ing = ",".join(l)
+    #print(ing)
+    url = "http://food2fork.com/api/search?key=17cd86aba7a553f7a8bb972f8066bafc&q=shrimp,mushroom"
+    url_req = urllib2.Request(url, headers={ 'User-Agent': 'Safari/537.36', 'Content-Type': 'application/json'} , method='GET')
+    response = urllib2.urlopen(url_req).read().decode('utf8')
+    response_json = json.loads(response) 
+    #print(response_json['recipes'])
+    print(response_json['recipes'][0]['title'], response_json['recipes'][0]['source_url'])
+    # for i in response_json['recipes']:
+    #     print(i['title'],i['source_url'])
+    #print(response_json)
+    #soup.body.findAll(text='Python')
+
+
 
 if __name__ == "__main__":
     main()
